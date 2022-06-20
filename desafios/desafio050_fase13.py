20c9dee2be1339d10ee84e37ea7afd2967f8ab4e
gabdev95 <gabriele.mdemedeiros@gmail.com>
2022-06-20 18:48:23 -0300

    Desafio 50 - Aula 13

--- desafios/desafio050_fase13.py
+++ desafios/desafio050_fase13.py
@@ -6,4 +6,4 @@ for c in range(0, 6):
     n = int(input('Digite um número: '))
     if n % 2 == 0:
         soma += n
-print('O somatório dos números pares que você digitou foi {}'.format(soma))
+print('O somatório dos números pares que você digitou foi {}.'.format(soma))
