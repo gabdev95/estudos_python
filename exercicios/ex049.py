5032b8773e33d2e6f175676d0119d6c0fa0e2f76
gabdev95 <gabriele.mdemedeiros@gmail.com>
2022-06-21 19:55:05 -0300

    Exercício 49 - Aula 13

---
+++ exercicios/ex049.py
@@ -0,0 +1,6 @@
+# Refaça o desafio 009, mostrando a tabuada de um número que o usuário
+# escolher, só que agora utilizando um laço for
+
+num = int(input('Digite um número para ver a tabuada dele: '))
+for c in range(1, 11):
+    print('{} x {:2} = {}'.format(num, c, num*c))
