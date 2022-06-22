dcc777ae3b2a248eff45745400d3cce064651e86
gabdev95 <gabriele.mdemedeiros@gmail.com>
2022-06-22 19:20:15 -0300

    Desafio 70 - Aula 15

---
+++ exercicios/ex071.py
@@ -0,0 +1,31 @@
+# Crie um programa que simule o funcionamento de um caixa eletrônico.
+# No início, pergunte ao usuário qual será o valor a ser sacado (numero inteiro)
+# e o progrma vai informar quantas cédulas de cada valor serão entregues.
+# OBS.: Considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.
+
+print('=' * 30)
+print('{:^30}'.format('BANCO GMM'))
+print('=' * 30)
+valor = int(input('Que valor você quer sacar? R$'))
+total = valor
+cedula = 50
+total_cedulas = 0
+while True:
+    if total >= cedula:
+        total -= cedula
+        total_cedulas += 1
+    else:
+        if total_cedulas > 0:
+            print(f'Total de {total_cedulas} cédulas de R${cedula}')
+        if cedula == 50:
+            cedula = 20
+        elif cedula == 20:
+            cedula = 10
+        elif cedula == 10:
+            cedula = 1
+        total_cedulas = 0
+        if total == 0:
+            break
+print('=' * 30)
+print('{:^30}'.format('VOLTE SEMPRE!'))
+print('=' * 30)
