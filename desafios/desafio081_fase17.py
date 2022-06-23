1dcf5c89c3fb21aa3e76494497fe72afb644a51e
gabdev95 <gabriele.mdemedeiros@gmail.com>
2022-06-23 18:26:53 -0300

    Desafio 81 - Aula 17

--- desafios/desafio081_fase17.py
+++ desafios/desafio081_fase17.py
@@ -16,8 +16,9 @@ while True:
     else:
         break
 valores.sort(reverse=True)
+print('-=' * 30)
 print(f'Foram digitados {len(valores)} números.')
-print(f'A lista ordenada: {valores}')
+print(f'Os valores em ordem decrescente são: {valores}')
 if 5 in valores:
     print('O valor 5 FOI DIGITADO e está na lista.')
 else:
