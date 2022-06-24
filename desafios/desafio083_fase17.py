60d3f062e04de94b00e16414db86cbad979e0f81
gabdev95 <gabriele.mdemedeiros@gmail.com>
2022-06-24 10:38:13 -0300

    Desafio e exercícios 83 - Aula 17

---
+++ desafios/desafio083_fase17.py
@@ -0,0 +1,19 @@
+# Crie um programa onde o usuário digite uma expressão qualquer
+# que use parênteses. Seu aplicativo deverá analisar se a expressão
+# passada está com os parênteses abertos ou fechados na ordem correta.
+
+expr = str(input('Digite a expressão: '))
+pilha = []
+for simbolo in expr:
+    if simbolo == '(':
+        pilha.append('(')
+    elif simbolo == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressão está válida!')
+else:
+    print('Sua expressão está errada!')
