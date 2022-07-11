48b60390d0dfe1bd8bf0c243be84eb46e978c9e9
gabdev95 <gabriele.mdemedeiros@gmail.com>
2022-07-11 18:40:46 -0300

    Atividade de cosseno

---
+++ cicero/Complexidade_calculoDoCosseno.py
@@ -0,0 +1,37 @@
+# Complexidade – Cálculo do cosseno através de uma série de valores com o
+# fatorial
+
+'''def fatorial(entrada_n):
+    if entrada_n <= 1:
+        return 1
+    else:
+        return entrada_n * fatorial(entrada_n - 1)
+
+def positivo_ou_negativo(entrada):
+    if entrada % 2 == 0:
+        return 1
+    else:
+        return -1
+
+
+def calcula_exponencial(base, expoente):
+    if expoente == 0:
+        return 1
+    else:
+        return base * calcula_exponencial(base, expoente - 1)'''
+
+
+def interacao(n, x):
+    for c in range(50):
+
+
+
+'''def calcularCosseno(x):
+
+
+import math
+x = float(input('>>> Digite a exp: '))
+print(calcula_exponencial(base, 2 * expoente))
+print(f'O cosseno de {x} é {calcularCosseno(x):.4f}')
+print(f'O cosseno de {x} é {math.cos(x):.4f}')'''
+
