05cf78d3da0510fa3f6c02d2bcc68741ee4084cc
gabdev95 <gabriele.mdemedeiros@gmail.com>
2022-07-12 10:25:02 -0300

    Atividade de cosseno

--- cicero/Complexidade_calculoDoCosseno.py
+++ cicero/Complexidade_calculoDoCosseno.py
@@ -1,37 +1,30 @@
 # Complexidade – Cálculo do cosseno através de uma série de valores com o
 # fatorial
 
-'''def fatorial(entrada_n):
-    if entrada_n <= 1:
+def fatorial(entrada):
+    if entrada <= 1:
         return 1
     else:
-        return entrada_n * fatorial(entrada_n - 1)
-
-def positivo_ou_negativo(entrada):
-    if entrada % 2 == 0:
-        return 1
-    else:
-        return -1
+        return entrada * fatorial(entrada - 1)
 
 
-def calcula_exponencial(base, expoente):
+def exponencial(base, expoente):
     if expoente == 0:
         return 1
     else:
-        return base * calcula_exponencial(base, expoente - 1)'''
+        return base * exponencial(base, expoente - 1)
 
 
-def interacao(n, x):
-    for c in range(50):
+def interacao(n, x):  # equação dada para o cálculo do cosseno
+    return exponencial(-1, n) / fatorial(2 * n) * exponencial(x, 2 * n)
 
 
+def calcula_cosseno(x):
+    cosx = 0
+    for n in range(50):  # loop de interações
+        cosx += interacao(n, x)  # incrementa o cosseno dentro das interações
+    return cosx
 
-'''def calcularCosseno(x):
 
-
-import math
-x = float(input('>>> Digite a exp: '))
-print(calcula_exponencial(base, 2 * expoente))
-print(f'O cosseno de {x} é {calcularCosseno(x):.4f}')
-print(f'O cosseno de {x} é {math.cos(x):.4f}')'''
-
+x = float(input(''))
+print(f'{calcula_cosseno(x):.4f}')
